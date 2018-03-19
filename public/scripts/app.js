1f9b8b8891a08861c4e559b55554c33bfd4f785c
ktso11 <sokatie11@gmail.com>
2018-03-19 09:32:13 -0700

    added map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,37 @@
+
+
 $(document).ready(function(){
 console.log("Sanity Check: JS is working!");
 
 
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: {lat: 37.77, lng: -122.44}
+    });
+    var geocoder = new google.maps.Geocoder();
 
+    document.getElementById('submit').addEventListener('click', function() {
+      geocodeAddress(geocoder, map);
+    });
+  }
 
+  function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
 
+  initMap();
 
 
 });
